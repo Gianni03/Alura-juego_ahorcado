d87fca3aaf06c22412c1302af9340188f6de589f
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2022-05-26 16:42:27 -0300

    estructura Html

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+palabras = {"alura"};
+estados = {1,2,3,4,5,6,7,G}; perdio a gano
+
+configuracion
+cadena a adivinar(palabra)
+estado lista de estados
+conjunto de letras - adivinadas
+conjunto de caracteres errados
+
+
+*/
