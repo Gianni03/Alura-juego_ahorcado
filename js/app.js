3638aee0459192d01e502daec607826883c0e560
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2022-06-01 20:59:44 -0300

    configurando JS

--- js/app.js
+++ js/app.js
@@ -10,3 +10,55 @@ conjunto de caracteres errados
 
 
 */
+(function(){   
+   let juego = {
+      palabra: "PALABRA",
+      estado: 7,
+      adivinado: ["A", "L"],
+      errado: ["K", "B", "J"]
+   }
+   
+   let $html = {
+      hombre: document.getElementById('hombre'),
+      adivinado: document.querySelector('.adivinado'),
+      errado: document.querySelector('.errado')
+   }
+   function dibujar(juego){
+      //actualiza la img
+      let $elem;
+      $elem = $html.hombre
+      $elem.src = './img/' + juego.estado + '.png'
+
+      // creamos las letras a adivinar
+      let palabra = juego.palabra;
+      let adivinado = juego.adivinado;
+      $elem = $html.adivinado;
+      for(let letra of palabra) {
+         let $span = document.createElement('span');
+         let $txt = document.createTextNode(' ')
+         if(adivinado.indexOf(letra) >= 0) {
+            $txt.nodeValue = letra
+         }
+         $span.setAttribute('class','l-correcta')
+         $span.appendChild($txt);
+         $elem.appendChild($span)
+      }
+      // letras erradas
+      let errado = juego.errado
+      $elem = $html.errado
+      for(let letra of errado) {
+         let $span = document.createElement('span');
+         let $txt = document.createTextNode(letra);
+         $span.setAttribute('class', 'l-error')
+         $span.appendChild($txt);
+         $elem.appendChild($span);
+      }
+   }
+   console.log(juego)
+
+   dibujar(juego)
+
+
+}()) 
+
+
