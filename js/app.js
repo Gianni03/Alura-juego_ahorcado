8992f230d6519c410bf589c3336ed4d1da35d15f
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2022-06-05 16:20:00 -0300

    terminando la seccion de letras del juego

--- js/app.js
+++ js/app.js
@@ -10,43 +10,51 @@ conjunto de caracteres errados
 
 
 */
-(function(){   
-   let juego = {
-      palabra: "PALABRA",
-      estado: 7,
-      adivinado: ["A", "L"],
-      errado: ["K", "B", "J"]
-   }
-   
-   let $html = {
+;
+(function () {
+   'use strict'
+
+   var palabras = ["AHORCADO", "TERMO", "CELULAR", "TOMATES", "ROSARIO", "AEROPUERTO"]
+
+   var juego = null;
+   var finalizado = false;
+
+   var $html = {
       hombre: document.getElementById('hombre'),
       adivinado: document.querySelector('.adivinado'),
       errado: document.querySelector('.errado')
    }
-   function dibujar(juego){
+
+   function dibujar(juego) {
       //actualiza la img
-      let $elem;
+      var $elem;
       $elem = $html.hombre
+      var estado = juego.estado;
+      if (estado == 8) {
+         estado = juego.previo
+      }
       $elem.src = './img/' + juego.estado + '.png'
 
       // creamos las letras a adivinar
-      let palabra = juego.palabra;
-      let adivinado = juego.adivinado;
+      var palabra = juego.palabra;
+      var adivinado = juego.adivinado;
       $elem = $html.adivinado;
-      for(let letra of palabra) {
+      $elem.innerHTML = "";
+      for (let letra of palabra) {
          let $span = document.createElement('span');
-         let $txt = document.createTextNode(' ')
-         if(adivinado.indexOf(letra) >= 0) {
-            $txt.nodeValue = letra
+         let $txt = document.createTextNode(' ');
+         if (adivinado.has(letra)) {
+            $txt.nodeValue = letra;
          }
-         $span.setAttribute('class','l-correcta')
+         $span.setAttribute('class', 'l-correcta')
          $span.appendChild($txt);
-         $elem.appendChild($span)
+         $elem.appendChild($span);
       }
       // letras erradas
-      let errado = juego.errado
+      var errado = juego.errado
       $elem = $html.errado
-      for(let letra of errado) {
+      $elem.innerHTML = ""
+      for (let letra of errado) {
          let $span = document.createElement('span');
          let $txt = document.createTextNode(letra);
          $span.setAttribute('class', 'l-error')
@@ -54,11 +62,93 @@ conjunto de caracteres errados
          $elem.appendChild($span);
       }
    }
-   console.log(juego)
-
-   dibujar(juego)
 
 
-}()) 
+   function adivinar(juego, letra) {
+      let estado = juego.estado;
+      // si ya se gano o perdio no hay que hacer nada
+      if (estado == 1 || estado == 8) {
+         return
+      }
+
+      var adivinado = juego.adivinado;
+      var errado = juego.errado;
+      // su se adivino o erro la letra nada que hacer
+      if (adivinado.has(letra) || errado.has(letra)) {
+         return
+      }
+
+      var palabra = juego.palabra;
+      var letras = juego.letras;
+
+      if (letras.has(letra)) {
+         // agregamos a la lista de letras adivinadas
+         adivinado.add(letra)
+         // actualizamos las letras restantes
+         juego.restante--
+
+         // Si ya se ha ganado, debemos indicarlo
+         if (juego.restante === 0) {
+            juego.previo = juego.estado
+            juego.estado = 8
+         }
+      } else {
+         // Si no es letra de la palabra, acercamos al hombre un paso más de su ahorca
+         juego.estado--
+         // Agregamos la letra, a la lista de letras erradas
+         errado.add(letra)
+      }
 
 
+
+   }
+
+
+   window.onkeypress = function adivinarLetra(e) {
+      var letra = e.key;
+      letra = letra.toUpperCase();
+      if (/[^A-ZÑ]/.test(letra)) {
+         return
+      }
+      adivinar(juego, letra)
+      var estado = juego.estado;
+      if (estado == 8 && !finalizado) {
+         alert("GANASTE");
+         finalizado = true;
+      }else if (estado == 1 && !finalizado) {
+         alert("PERDISTE");
+         finalizado = true;
+      }
+      dibujar(juego)
+   }
+
+
+   window.nuevoJuego = function nuevoJuego() {
+      var palabra = palabraAleatoria()
+      juego = {};
+      juego.palabra = palabra;
+      juego.estado = 7;
+      juego.adivinado = new Set();
+      juego.errado = new Set();
+      finalizado = false;
+
+      var letras = new Set()
+      for (let letra of palabra){
+         letras.add(letra);
+      }
+      juego.letras = letras;
+      juego.restante = letras.size;
+
+      dibujar(juego);
+
+
+   }
+
+   function palabraAleatoria() {
+      let index = ~~(Math.random() * palabras.length)
+      return palabras[index];
+   }
+
+   nuevoJuego()
+
+}())
